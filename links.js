8d669b148276e49ddaea436c664cd9a97a94e729
Wyatt Wilson <95758153+DeterioratingSanity@users.noreply.github.com>
2023-03-27 11:50:40 -0400

    Update links.js

--- links.js
+++ links.js
@@ -4,3 +4,11 @@ $('class name').click(function(){
   return false;
 });
 */
+$('#one').click(function(){
+  window.open('./yearbookstaff.html', '_self');
+  return false;
+});
+$('#two').click(function(){
+  window.open('./journpathway.html', '_self');
+  return false;
+});
